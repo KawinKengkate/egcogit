14547e2a1f9b29fb3d995d3c32f302d5881cedb4
kawin <kawin.ken@student.mahidol.ac.th>
2024-04-03 16:37:40 +0700

    update app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World Here are the update version'))
+app.get('/', (req, res) => res.send('Hello World, My name is Kawin Kengkate.'))
 
 app.listen(3000, () => console.log('server start'))

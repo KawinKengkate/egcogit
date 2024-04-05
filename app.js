74561e0584090aa3056569fceef24c4408f1396c
kawin <kawin.ken@student.mahidol.ac.th>
2024-04-05 15:45:01 +0700

    test github action

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World, My name is Kawin Kengkate.'))
+app.get('/', (req, res) => res.send('Hello World, My name is Kawin Kengkate.\nTest github Action.'))
 
 app.listen(3000, () => console.log('server start'))
